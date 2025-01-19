acef0f69c9d7e0d08a8cad2090a12f425f05665c
Peter <peters.oluochs@gmail.com>
2025-01-20 00:03:52 +0300

    updated by clarifying the instructions

--- scitt.py
+++ scitt.py
@@ -1,4 +1,5 @@
-"""  Please make sure you read the entire README.md file to follow the instructions """
+"""  Please make sure you read the entire README.md file to follow the instructions. 
+You dont have to follow how I have started the game. Please use your creativity """
 
 print("Welcome to the Number Guessing Game!")
 pass
